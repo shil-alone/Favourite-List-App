53d641ab23efaef0671eec2a86170058b0e64e65
shil-alone <shilalone007@gmail.com>
2021-08-01 20:02:24 +0530

    added interface to handle clicks on category

--- app/src/main/java/com/codershil/favouritelistapp/CategoryRecyclerAdapter.java
+++ app/src/main/java/com/codershil/favouritelistapp/CategoryRecyclerAdapter.java
@@ -12,11 +12,17 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryViewHolder> {
-    ArrayList<Category> categories = new ArrayList<>();
+public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryViewHolder>{
+    interface CategoryClickListener {
+        void categoryIsClicked(Category category);
+    }
 
-    public CategoryRecyclerAdapter(ArrayList<Category> categories){
+    ArrayList<Category> categories = new ArrayList<>();
+    CategoryClickListener categoryClickListener;
+
+    public CategoryRecyclerAdapter(ArrayList<Category> categories, CategoryClickListener categoryClickListener) {
         this.categories = categories;
+        this.categoryClickListener = categoryClickListener;
     }
 
 
@@ -24,15 +30,22 @@ public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryViewHo
     @Override
     public CategoryViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.category_item,parent,false);
+        View view = layoutInflater.inflate(R.layout.category_item, parent, false);
         CategoryViewHolder categoryViewHolder = new CategoryViewHolder(view);
         return categoryViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull CategoryViewHolder holder, int position) {
-        holder.getTxtCategoryNumber().setText(String.valueOf(position+1));
+        holder.getTxtCategoryNumber().setText(String.valueOf(position + 1));
         holder.getTxtCategoryName().setText(categories.get(position).getName());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                categoryClickListener.categoryIsClicked(categories.get(holder.getAdapterPosition()));
+            }
+        });
+
     }
 
     @Override
@@ -41,8 +54,8 @@ public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryViewHo
     }
 
 
-    public void addCategory(Category category){
+    public void addCategory(Category category) {
         categories.add(category);
-        notifyItemChanged(categories.size()-1);
+        notifyItemChanged(categories.size() - 1);
     }
 }
