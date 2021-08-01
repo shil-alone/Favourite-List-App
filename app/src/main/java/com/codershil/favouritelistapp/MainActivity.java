93107d2598252565f184b2a85a042292f9894bf4
shil-alone <shilalone007@gmail.com>
2021-08-01 14:42:39 +0530

    added view categories and create category feature

--- app/src/main/java/com/codershil/favouritelistapp/MainActivity.java
+++ app/src/main/java/com/codershil/favouritelistapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.codershil.favouritelistapp;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.InputType;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -19,12 +22,16 @@ import com.codershil.favouritelistapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     private RecyclerView recyclerView;
+    CategoryManager categoryManager = new CategoryManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,31 +42,56 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.category_recyclerview);
 
-        recyclerView.setAdapter(new CategoryRecyclerAdapter());
+        ArrayList<Category> categories = new ArrayList<>();
+        recyclerView.setAdapter(new CategoryRecyclerAdapter(categories));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayCreateCategoryDialog();
+            }
+        });
 
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void displayCreateCategoryDialog() {
+        String title = getString(R.string.create_category_title);
+        String positiveButtonText = getString(R.string.positive_button_text);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        EditText edtCategoryText = new EditText(this);
+        edtCategoryText.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(edtCategoryText);
+        builder.setTitle(title);
+        builder.setPositiveButton(positiveButtonText, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Category category = new Category(edtCategoryText.getText().toString(), new ArrayList<String>());
+                categoryManager.saveCategory(category);
+
+                CategoryRecyclerAdapter categoryRecyclerAdapter = (CategoryRecyclerAdapter) recyclerView.getAdapter();
+                categoryRecyclerAdapter.addCategory(category);
+
+            }
+        });
+        builder.create().show();
     }
 
 }
