4359224b465b2def5c9e528ecca7acf93bc99c37
shil-alone <shilalone007@gmail.com>
2021-08-01 20:01:48 +0530

    set the title of action bar

--- app/src/main/java/com/codershil/favouritelistapp/CategoryItemsActivity.java
+++ app/src/main/java/com/codershil/favouritelistapp/CategoryItemsActivity.java
@@ -10,5 +10,7 @@ public class CategoryItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_items);
+        Category category =(Category) getIntent().getSerializableExtra(MainActivity.CATEGORY_OBJECT_KEY);
+        setTitle(category.getName());
     }
 }
