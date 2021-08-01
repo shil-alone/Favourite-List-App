9badb002f1787986977c20988533012c1d057ceb
shil-alone <shilalone007@gmail.com>
2021-08-01 14:41:03 +0530

    retrived data from sharedPreferences

--- app/src/main/java/com/codershil/favouritelistapp/Category.java
+++ app/src/main/java/com/codershil/favouritelistapp/Category.java
@@ -1,2 +1,23 @@
-package com.codershil.favouritelistapp;public class Category {
+package com.codershil.favouritelistapp;
+
+import java.util.ArrayList;
+
+public class Category {
+
+    private String name;
+    private ArrayList<String> items = new ArrayList<>();
+
+    public Category(String name ,ArrayList<String> items){
+        this.name = name;
+        this.items = items;
+    }
+
+    String getName(){
+        return name;
+    }
+
+    ArrayList<String> getItems(){
+        return items;
+    }
+
 }
--- app/src/main/java/com/codershil/favouritelistapp/CategoryManager.java
+++ app/src/main/java/com/codershil/favouritelistapp/CategoryManager.java
@@ -1,4 +1,38 @@
 package com.codershil.favouritelistapp;
 
-public class CateogoryManager {
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+
+public class CategoryManager {
+
+    private Context context;
+    public CategoryManager(Context context){
+        this.context = context;
+    }
+
+    public void saveCategory(Category category){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        HashSet itemHashSet = new HashSet(Arrays.asList(category.getItems()));
+        editor.putStringSet(category.getName(),itemHashSet);
+        editor.apply();
+    }
+
+    public ArrayList<Category> retrieveCategories(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        Map<String,?> categoriesMap = sharedPreferences.getAll();
+        ArrayList<Category> categories = new ArrayList<>();
+        for (Map.Entry<String,?> entry : categoriesMap.entrySet()){
+            Category category = new Category(entry.getKey(),new ArrayList<String>((HashSet)entry.getValue()));
+            categories.add(category);
+        }
+        return categories;
+    }
 }
